6842c0909852bfb77a6ad0086958740c15435943
Nnaemeka10 <egbuchannaemeka2002@gmail.com>
2025-09-23 10:41:38 +0100

    added some refactor to welcome email

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -2,6 +2,13 @@ import { resendClient, sender } from "../lib/resend.js";
 import { createWelcomeEmailTemplate } from "./emailTemplates.js";
 
 export const sendWelcomeEmail = async (email,name,clientURL) => {
+    if(!sender?.email || !sender?.name) {
+        throw new Error("Emaail sender configuration is missing(name or email)");
+    }
+
+    if(!email) throw new Error("Recipient email is required");
+    if(!clientURL) throw new Error("Client URL is required");
+
     const { data, error} = await resendClient.emails.send({
         from: `${sender.name} <${sender.email}>`,
         to: email,
