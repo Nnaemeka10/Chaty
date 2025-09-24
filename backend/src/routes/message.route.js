c4fa6d353a69e3b38b3f3445f5edc4da07108000
Nnaemeka10 <egbuchannaemeka2002@gmail.com>
2025-09-24 19:13:31 +0100

    message-routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } from '../controllers/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+
+
 
 const router = express.Router();
+//middleware executes in order, rate limiter first, then authentication
 
-router.get('/send', (req, res) => {
-    res.send('Send message endpoint');
-})
+router.use(arcjetProtection, protectRoute);
+
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessageByUserId);
+router.post('/send/:id', sendMessage);
 
 export default router;
